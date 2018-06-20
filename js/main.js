58ce413a1a0b6de32509c6e6bda3735796327cca
Nurit Levi <nuritlh@gmail.com>
2018-06-20 19:12:07 +0300

    meme gallery n key filter

--- js/main.js
+++ js/main.js
@@ -1,24 +1,52 @@
-'use strict'
+'use strict';
 
 function init() {
-    var imgs = getImgsForDisplay()
-    renderImgs(imgs)
-    renderKeywords()
+  var imgs = getImgsForDisplay('All');
+  var keywords = getketwordsForDisplay();
+  renderImgs(imgs);
+  renderKeywords(keywords);
+  renderFilter(keywords);
 }
 
 function renderImgs(imgs) {
+  var strHTML = imgs.map(function(img) {
+    return `
+    <img class="img-gallery" src="img/${img.id}.jpg">
+        `;
+  });
 
+  document.querySelector('.gallery').innerHTML = strHTML.join('');
 }
 
-function renderKeywords() {
+function renderKeywords(keywords) {
+  var strHTML = '';
 
+  for (var key in keywords) {
+    strHTML += `<li class="tag-word" style="font-size: ${keywords[key] *
+      10}px" onclick="onTagCliked('${key}')">
+    ${key}
+</li>`;
+  }
+
+  document.querySelector('.keywords').innerHTML = strHTML;
+}
+
+function renderFilter(keywords) {
+  var strHTML = '';
+  for (var key in keywords) {
+    strHTML += `<option >${key} </option>`;
+  }
+  document.querySelector('.input-option').innerHTML = strHTML;
+}
+
+function onTagCliked(strTag) {
+  var imgs = getImgsForDisplay(strTag);
+  renderImgs(imgs);
 }
 
 function openModal(id) {
-    $('.editor').toggle('hidden');
-    renderCanvas(id)
+  $('.editor').toggle('hidden');
+  renderCanvas(id);
 }
 
-function renderCanvas(id) {
-
-}
+function renderCanvas(id) {}
--- js/memes-service.js
+++ js/memes-service.js
@@ -1,55 +1,66 @@
-'use strict'
+'use strict';
+var gkerWord = 'All';
 
-var gImgs = [{ id: 1, url: 'img/001.jpg', keywords: ['happy'] },
-             { id: 2, url: 'img/002.jpg', keywords: ['happy', 'success'] },
-             { id: 3, url: 'img/003.jpg', keywords: ['trump', 'politics'] },
-             { id: 4, url: 'img/004.jpg', keywords: ['animals', 'dogs'] },
-             { id: 5, url: 'img/005.jpg', keywords: ['animals', 'dogs', 'babies'] },
-             { id: 6, url: 'img/006.jpg', keywords: ['animals', 'cats'] },
-             { id: 7, url: 'img/007.jpg', keywords: ['funny'] },
-             { id: 8, url: 'img/008.jpg', keywords: ['babies'] },
-             { id: 9, url: 'img/009.jpg', keywords: ['israeli'] },
-             { id: 10, url: 'img/010.jpg', keywords: ['shocked'] },
-             { id: 11, url: 'img/011.jpg', keywords: ['funny', 'aliens'] },
-             { id: 12, url: 'img/012.jpg', keywords: ['funny'] },
-             { id: 13, url: 'img/013.jpg', keywords: ['babies', 'dancing'] },
-             { id: 14, url: 'img/014.jpg', keywords: ['trump', 'politics'] },
-             { id: 15, url: 'img/015.jpg', keywords: ['babies', 'shocked'] },
-             { id: 16, url: 'img/016.jpg', keywords: ['animals', 'dogs'] },
-             { id: 17, url: 'img/017.jpg', keywords: ['obama','politics', 'funny'] },
-             { id: 18, url: 'img/018.jpg', keywords: ['love'] },
-             { id: 19, url: 'img/019.jpg', keywords: ['happy', 'success', 'leo'] },
-             { id: 20, url: 'img/020.jpg', keywords: ['serious'] },
-             { id: 21, url: 'img/021.jpg', keywords: ['funny'] },
-             { id: 22, url: 'img/022.jpg', keywords: ['oprah'] },
-             { id: 23, url: 'img/023.jpg', keywords: ['shocked'] },
-             { id: 24, url: 'img/024.jpg', keywords: ['putin', 'serious', 'politics'] },
-             { id: 25, url: 'img/025.jpg', keywords: ['cartoon'] }
+var gImgs = [
+  { id: 1, url: 'img/1.jpg', keywords: ['happy'] },
+  { id: 2, url: 'img/2.jpg', keywords: ['happy', 'success'] },
+  { id: 3, url: 'img/3.jpg', keywords: ['trump', 'politics'] },
+  { id: 4, url: 'img/4.jpg', keywords: ['animals', 'dogs'] },
+  { id: 5, url: 'img/5.jpg', keywords: ['animals', 'dogs', 'babies'] },
+  { id: 6, url: 'img/6.jpg', keywords: ['animals', 'cats'] },
+  { id: 7, url: 'img/7.jpg', keywords: ['funny'] },
+  { id: 8, url: 'img/8.jpg', keywords: ['babies'] },
+  { id: 9, url: 'img/9.jpg', keywords: ['israeli'] },
+  { id: 10, url: 'img/10.jpg', keywords: ['shocked'] },
+  { id: 11, url: 'img/11.jpg', keywords: ['funny', 'aliens'] },
+  { id: 12, url: 'img/12.jpg', keywords: ['funny'] },
+  { id: 13, url: 'img/13.jpg', keywords: ['babies', 'dancing'] },
+  { id: 14, url: 'img/14.jpg', keywords: ['trump', 'politics'] },
+  { id: 15, url: 'img/15.jpg', keywords: ['babies', 'shocked'] },
+  { id: 16, url: 'img/16.jpg', keywords: ['animals', 'dogs'] },
+  { id: 17, url: 'img/17.jpg', keywords: ['obama', 'politics', 'funny'] },
+  { id: 18, url: 'img/18.jpg', keywords: ['love'] },
+  { id: 19, url: 'img/19.jpg', keywords: ['happy', 'success', 'leo'] },
+  { id: 20, url: 'img/20.jpg', keywords: ['serious'] },
+  { id: 21, url: 'img/21.jpg', keywords: ['funny'] },
+  { id: 22, url: 'img/22.jpg', keywords: ['oprah'] },
+  { id: 23, url: 'img/23.jpg', keywords: ['shocked'] },
+  { id: 24, url: 'img/24.jpg', keywords: ['putin', 'serious', 'politics'] },
+  { id: 25, url: 'img/25.jpg', keywords: ['cartoon'] }
 ];
 
-var gMeme = {
-    selectedImgId: 5,
-    txts: [
-        {
-            line: 'I never eat Falafel',
-            size: 20,
-            align: 'left',
-            color: 'red',
-            cordX: 0,
-            cordY: 0
-        }
-    ]
+var gKeywords = createKeywords(gImgs);
+
+function createKeywords() {
+  var kerwords = gImgs.reduce(function(acc, img) {
+    img.keywords.forEach(function(tag) {
+      if (acc[tag]) {
+        acc[tag] = acc[tag] + 1;
+      } else acc[tag] = 1;
+    });
+    return acc;
+  }, {});
+  console.log(kerwords);
+
+  return kerwords;
 }
 
-var gKeywords = [{tagName: 'happy', rate: 2 },
-                 {tagName: 'cats', rate: 4 },
-                 {tagName: 'funny', rate: 5 },
-                 {tagName: 'babies', rate:2 },
-                 {tagName: 'dogs', rate:1 },
-]
+function getImgsForDisplay(strFliter) {
+  var imgs = [];
+  if (strFliter === 'All') {
+    imgs = gImgs;
+  } else {
+    imgs = gImgs.filter(function(img) {
+      return img.keywords.some(function(tag) {
+        return tag === strFliter;
+      });
+    });
+  }
+  console.log(imgs);
 
-function getImgsForDisplay() {
-
-
+  return imgs;
 }
 
+function getketwordsForDisplay() {
+  return gKeywords;
+}
